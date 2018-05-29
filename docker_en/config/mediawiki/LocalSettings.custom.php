15444ab5a716d6935a72aa55034f010defcf3a8a
Florent Kaisser <contact@lille.bike>
2018-05-29 22:31:31 +0200

    add wikifundi en

---
+++ docker_en/config/mediawiki/LocalSettings.custom.php
@@ -0,0 +1,19 @@
+<?php
+
+# General
+$wgLanguageCode     = "en";
+$wgSitename         = "Kiwix";
+
+# Site language 
+$wgLanguageCode = "en";
+$wgUploadWizardConfig['uwLanguages'] = array( 'en' => 'English' );
+
+# Database settings
+$wgDBtype        = "sqlite";
+$wgDBserver      = "";
+$wgDBname        = "mw_kiwix_en";
+$wgDBuser        = "";
+$wgDBpassword    = "";
+$wgSQLiteDataDir = "/var/www/data";
+
+?>
