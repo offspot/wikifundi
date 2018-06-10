4c088f9806f952053d4cc5d6d2a96fa9b199e7dd
Florent Kaisser <contact@lille.bike>
2018-06-10 13:02:28 +0200

    set maximum upload file size to 5M and generate pywikibot config file password with specific password

--- docker-wikifundi_en/config/mediawiki/LocalSettings.custom.php
+++ docker-wikifundi_en/config/mediawiki/LocalSettings.custom.php
@@ -19,4 +19,10 @@ $wgSQLiteDataDir = "/var/www/data";
 # Custom logo
 $wgLogo = "logo.png";
 
+# Uploads
+ini_set('memory_limit', '512M');
+ini_set('post_max_size', '5M');
+ini_set('upload_max_filesize', '5M');
+$wgMaxUploadSize = 1024*1024*5;
+
 ?>
--- docker-wikifundi_en/config/pywikibot/user-password.py
+++
@@ -1,2 +0,0 @@
-# -*- coding: utf-8 -*-
-(u'Admin', BotPassword(u'sync', u'8no4evs3dnpvd4ardg1n7fp8gcdaon4u'))
