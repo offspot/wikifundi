3ddc433dc47a7df1a41db082560959e435773a67
Florent Kaisser <contact@lille.bike>
2018-06-22 16:19:10 +0200

    no include logs in tarball

--- wikifundi-en/export_data.php
+++ wikifundi-en/export_data.php
@@ -3,7 +3,7 @@
   header("Content-Type: application/x-bzip2");
   header("Content-Disposition: attachment; filename=\"wikifundi-data.tar.bz\"");
 
-  passthru("tar -C /var/www/ -cj data",$err);
+  passthru("tar -C /var/www/ -cj --exclude=data/log* data",$err);
   
   exit();
   
