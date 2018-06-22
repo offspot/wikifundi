9f8d32b67411fe95db598c6aa84b94aac0ffd6ff
Florent Kaisser <contact@lille.bike>
2018-06-22 16:51:20 +0200

    add date and language code in tarball filename

--- wikifundi-en/export_data.php
+++ wikifundi-en/export_data.php
@@ -1,10 +1,18 @@
 <?php
 
+  $wgSitename = "mywiki";
+  $wgLanguageCode = "all";
+
+  include 'w/LocalSettings.custom.php';
+  
+  $filename=strtolower($wgSitename."-".$wgLanguageCode."_".date('Y-m-d').".tar.bz2");
+
   header("Content-Type: application/x-bzip2");
-  header("Content-Disposition: attachment; filename=\"wikifundi-data.tar.bz\"");
+  header("Content-Disposition: attachment; filename=\"".$filename."\"");
 
   passthru("tar -C /var/www/ -cj --exclude=data/log* data",$err);
   
+
   exit();
   
 ?>
