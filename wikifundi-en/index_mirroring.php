499f4aaec82a3a4aa70d59e1d05fad313d18db98
Florent Kaisser <contact@lille.bike>
2018-06-30 04:39:22 +0200

    update mirroring index

--- wikifundi-en/index_mirroring.php
+++ wikifundi-en/index_mirroring.php
@@ -1,7 +1,12 @@
 <?php
-
-  echo '<h2>Mirroring in progress ...</h2>';
-
-  echo '<a href="../w/data/log/mirroring.log">Log</a>';
-
+header("HTTP/1.0 503 Mirroring in progress ...");
 ?>
+<html>
+<h1><a href="http://www.wikifundi.org"><img height="128" src="http://www.wikifundi.org/wp-content/uploads/2016/09/WikiFundi-logo.png"></img></a></h1>
+<h2>Mirroring in progress ...</h2>
+<body>
+<p>
+        <a href="../w/data/log/mirroring.log">Log</a>
+</p>
+</body>
+</html>
