5de64c9596f5b90422e3f925496ddebeee433e5e
Florent Kaisser <contact@lille.bike>
2018-07-03 10:15:22 +0200

    update limite file size at 100MB

--- wikifundi-en/config/mediawiki/LocalSettings.mirroring.php
+++ wikifundi-en/config/mediawiki/LocalSettings.mirroring.php
@@ -2,8 +2,8 @@
 
 # Uploads
 ini_set('memory_limit', '512M');
-ini_set('post_max_size', '20M');
-ini_set('upload_max_filesize', '20M');
-$wgMaxUploadSize = 1024*1024*20;
+ini_set('post_max_size', '100M');
+ini_set('upload_max_filesize', '100M');
+$wgMaxUploadSize = 1024*1024*100;
 
 ?>
