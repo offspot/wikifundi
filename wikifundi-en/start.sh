b7881b6737299ce09b4404f2db9c3db56e8b598d
Florent Kaisser <contact@lille.bike>
2018-07-06 06:37:48 +0200

    add CLEAN IMAGE option

--- wikifundi-en/start.sh
+++ wikifundi-en/start.sh
@@ -5,12 +5,6 @@ DATABASE_FILE=${DATA_DIR}/${DATABASE_NAME}.sqlite
 README_FILE=${DATA_DIR}/README
 CFG_DIR=${DATA_DIR}/config
 
-function clean {
-  echo "Delete thumb, temp and archive ..."
-  rm -rvf ${DATA_DIR}/images/thumb/* ${DATA_DIR}/images/temp/* ${DATA_DIR}/images/archive/*
-  cp -f ./LocalSettings.custom.origin.php ${CFG_DIR}/LocalSettings.custom.php
-}
-
 cp README ${README_FILE} 
 
 mediawiki-init.sh
@@ -20,16 +14,16 @@ ln -s ${DATA_DIR} data
 if [ ${MIRRORING} ]
 then
 
-  echo "Start services ..."
-  service memcached start 
-  service php7.0-fpm start
-  service nginx start
-
   #Allow to write on database
   chmod 644 ${DATABASE_FILE}  
   
   echo "Start Mediawiki maintenance ..."
   maintenance/update.php --quick > ${LOG_DIR}/mw_update.log 
+  
+  echo "Start services ..."
+  service memcached start 
+  service php7.0-fpm start
+  service nginx start  
 
   # show mirroring page in index
   ln -fs index_mirroring.php index.php 
@@ -40,8 +34,10 @@ then
   # restore index
   ln -fs index_mediawiki.php index.php 
   
-  # purge cache
-  service memcached restart
+  echo "Stop services ..."
+  service nginx stop  
+  service php7.0-fpm stop
+  service memcached stop 
   
   # force to purge page cache
   touch LocalSettings.php
@@ -55,16 +51,9 @@ then
   #To write in image dir
   chown -R www-data:www-data ${DATA_DIR}  
   
-  service memcached stop 
-  service php7.0-fpm stop
-  service nginx stop  
-  
 fi
 
-if [ ${IMAGE_OVERSIZE} ]
-then 
-  find ${DATA_DIR}/images -size +${IMAGE_OVERSIZE}M -exec rm -f {} \;
-fi
+
 
 if [ ${CLEAN} ]
 then
@@ -74,6 +63,18 @@ then
   php maintenance/deleteArchivedFiles.php --delete  
   echo "Start Mediawiki maintenance ..."
   maintenance/update.php --quick
+fi
+
+if [ ${CLEAN_IMAGES} ]
+then
+  echo "Delete thumb, temp and archive ..."
+  rm -rvf ${DATA_DIR}/images/thumb/* ${DATA_DIR}/images/temp/* ${DATA_DIR}/images/archive/*
+  cp -f ./LocalSettings.custom.origin.php ${CFG_DIR}/LocalSettings.custom.php
+fi
+
+if [ ${IMAGE_OVERSIZE} ]
+then 
+  find ${DATA_DIR}/images -size +${IMAGE_OVERSIZE}M -exec rm -f {} \;
 fi
 
 if [ ! ${DEBUG}  ]
