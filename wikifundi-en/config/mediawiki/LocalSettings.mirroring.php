2fee7cf7b8a1a8931083f06eb6c73b0ed358074a
renaud gaudin <rgaudin@gmail.com>
2021-09-01 12:05:59 +0000

    special mirroring perms for sysop

--- wikifundi-en/config/mediawiki/LocalSettings.mirroring.php
+++ wikifundi-en/config/mediawiki/LocalSettings.mirroring.php
@@ -1,5 +1,20 @@
 <?php
 
+$wgGroupPermissions["sysop"]["*"] = true;
+$wgGroupPermissions["sysop"]["read"] = true;
+$wgGroupPermissions["sysop"]["createtalk"] = true;
+$wgGroupPermissions["sysop"]["createpage"] = true;
+$wgGroupPermissions["sysop"]["move-rootuserpages"] = true;
+$wgGroupPermissions["sysop"]["movefile"] = true;
+$wgGroupPermissions["sysop"]["move-categorypages"] = true;
+$wgGroupPermissions["sysop"]["protect"] = true;
+$wgGroupPermissions["sysop"]["editsitecss"] = true;
+$wgGroupPermissions["sysop"]["editsitejson"] = true;
+$wgGroupPermissions["sysop"]["editsitejs"] = true;
+$wgGroupPermissions["sysop"]["editusercss"] = true;
+$wgGroupPermissions["sysop"]["edituserjson"] = true;
+$wgGroupPermissions["sysop"]["edituserjs"] = true;
+
 # Uploads
 ini_set('memory_limit', '512M');
 ini_set('post_max_size', '200M');
